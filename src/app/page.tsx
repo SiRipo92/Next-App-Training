0c6206d221cc20fd1a432c23fe4d40a1a94a57a3
SiRipo92 <ripochesierra@gmail.com>
2025-02-23 21:47:53 +0100

    Updated main menu component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 import Head from "next/head";
 import Image from "next/image";
 import Header from "@/templates/Header/Header";
+import Hero from '@/components/hero'
 
 export default function Home() {
   return (
@@ -11,10 +12,10 @@ export default function Home() {
         <title>My Home Page</title>
         <meta name="description" content="My home page" />
         <link rel="icon" href="/favicon.ico" />
-        
       </Head>
       <Header />
         <main className="main">
+          <Hero />
       </main>
     </div>
   );
---
+++ src/components/hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+    
+  return (
+    <section className="hero w-full h-5 flex-1 flex justify-center">
+        <>
+        <div className="relative lg:bg-scroll">
+            <h1>
+                <p>This is where I write some text</p>
+            </h1>
+        </div>
+        </>
+    </section>
+  )
+}
+
+export default Hero
